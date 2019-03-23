fe7a767e4108f998cfdd113b85ed8c048d5743a0
kaiserawu <kaiser.a.wu@gmail.com>
2019-03-23 10:37:38 -0700

    Implemented basic tab and card logic

--- lambdatimes/src/components/Content/Tabs.js
+++ lambdatimes/src/components/Content/Tabs.js
@@ -1,5 +1,9 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import Tab from './Tab';
+
+let tabId = 0;
+
 const Tabs = props => {
   return (
     <div className="tabs">
@@ -7,10 +11,15 @@ const Tabs = props => {
         <span className="title">TRENDING TOPICS:</span>
         {/* map over the tabs provided on your props, create a new Tab component for each one.
             give the tab component a `selectTabHandler`, the `selectedTab`, and the `tab` itself as props*/}
+        {props.tabs.map(tab => <Tab key={tabId++} tab={tab} selectedTab={''} />)}
       </div>
     </div>
   );
 };
 
 // Make sure to use PropTypes to validate your types!
+Tabs.propTypes = {
+  tabs: PropTypes.arrayOf(PropTypes.string)
+}
+
 export default Tabs;
