97dac1dd62b5d618bb84b082f555c36c229054f7
kaiserawu <kaiser.a.wu@gmail.com>
2019-03-23 11:39:58 -0700

    Remove unneeded comments

--- lambdatimes/src/components/Content/Cards.js
+++ lambdatimes/src/components/Content/Cards.js
@@ -7,14 +7,11 @@ let cardId = 0;
 const Cards = props => {
   return (
     <div className="cards-container">
-      {/* Using the cards prop, map over the list creating a 
-          new Card component for each passing the card as the only prop*/}
       {props.cards.map(card => <Card key={cardId++} card={card} />)}
     </div>
   )
 }
 
-// Make sure you include prop types for all of your incoming props
 Cards.propTypes = {
   cards: PropTypes.arrayOf(PropTypes.shape({
     headline: PropTypes.string,
--- lambdatimes/src/components/Content/Tabs.js
+++ lambdatimes/src/components/Content/Tabs.js
@@ -9,15 +9,12 @@ const Tabs = props => {
     <div className="tabs">
       <div className="topics">
         <span className="title">TRENDING TOPICS:</span>
-        {/* map over the tabs provided on your props, create a new Tab component for each one.
-            give the tab component a `selectTabHandler`, the `selectedTab`, and the `tab` itself as props*/}
         {props.tabs.map(tab => <Tab key={tabId++} tab={tab} selectedTab={props.selectedTab} selectTabHandler={props.selectTabHandler} />)}
       </div>
     </div>
   );
 };
 
-// Make sure to use PropTypes to validate your types!
 Tabs.propTypes = {
   tabs: PropTypes.arrayOf(PropTypes.string),
   selectedTab: PropTypes.string,
